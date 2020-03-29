37e046bd7e2223529dab763ab36a67a22c80fec5
Priyansh Balyan <balyan.priyansh@mahindra.com>
2020-03-29 20:32:49 +0530

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,219 @@
+<template>
+  <div class="container">
+    <div>
+      <canvas id="canvas" :width="width" :height="height" />
+    </div>
+  </div>
+</template>
+
+<script>
+import * as d3 from 'd3'
+import * as topojson from 'topojson-client'
+import * as versor from 'versor'
+import * as scrapeIt from 'scrape-it'
+import world110m from '~/data/world-110m.json'
+
+export default {
+  components: {
+  },
+  data () {
+    return {
+      height: 800,
+      width: 800,
+      projection: d3.geoOrthographic().precision(0.1),
+      context: null,
+      path: null,
+      sphere: { type: 'Sphere' },
+      country: null,
+      countryData: []
+    }
+  },
+  async mounted () {
+    this.generateMap()
+    await this.loadData()
+  },
+  methods: {
+    async loadData () {
+      const options = {
+        countries: {
+          listItem: 'tr',
+          data: {
+            country: 'td > a.mt_a',
+            totalCases: 'td:nth-child(2)',
+            newCases: 'td:nth-child(3)',
+            totalDeaths: 'td:nth-child(4)',
+            newDeaths: 'td:nth-child(5)'
+          }
+        }
+      }
+      const { data } = await scrapeIt('https://cors-anywhere.herokuapp.com/https://www.worldometers.info/coronavirus/', options)
+      this.countryData = data.countries
+    },
+    generateMap () {
+      this.context = document.getElementById('canvas').getContext('2d')
+      this.path = d3.geoPath(this.projection, this.context)
+
+      const [[x0, y0], [x1, y1]] = d3.geoPath(this.projection.fitWidth(this.width, this.sphere)).bounds(this.sphere)
+      this.height = Math.ceil(y1 - y0)
+      const l = Math.min(Math.ceil(x1 - x0), this.height)
+      this.projection.scale(this.projection.scale() * (l - 1) / l).precision(0.2)
+
+      const borders = topojson.mesh(world110m, world110m.objects.countries, (a, b) => a !== b)
+      const countries = topojson.feature(world110m, world110m.objects.countries).features
+      const land = topojson.feature(world110m, world110m.objects.land)
+
+      d3.select(this.context.canvas)
+        .call(this.drag(this.projection)
+          .on('drag.render', () => this.render(land, borders, countries))
+          .on('end.render', () => this.render(land, borders, countries)))
+        .call(() => this.render(land, borders, countries))
+        .on('mousemove', () => this.mousemove(this.context.canvas, countries, land, borders))
+        .node()
+
+      d3.select('body').append('div')
+        .append('div')
+        .attr('id', 'tooltip')
+        .attr('style', 'position: absolute; opacity: 0;')
+    },
+
+    mousemove (canvas, countries, land, borders) {
+      const country = this.getCountry(d3.mouse(canvas), countries)
+      if (country) {
+        this.render(land, borders, country)
+        this.country = country.properties.name
+        const found = this.countryData.find(x => x.country === country.properties.name)
+        this.renderTooltip(found)
+      } else {
+        this.country = null
+        d3.select('#tooltip')
+          .style('opacity', 0)
+      }
+    },
+
+    renderTooltip (found) {
+      if (found) {
+        d3.select('#tooltip')
+          .style('left', (d3.event.pageX + 10) + 'px')
+          .style('top', (d3.event.pageY + 10) + 'px')
+          .style('opacity', 1)
+          .html(`<strong>${this.country}</strong>
+            <br>Total Cases: ${found.totalCases || 'N/A'}
+            <br>New Cases: ${found.newCases || 'N/A'}
+            <br>Total Deaths: ${found.totalDeaths || 'N/A'}
+            <br>New Deaths: <span style="color:#f00">${found.newDeaths || 'N/A'}</span>`)
+      } else if (this.country) {
+        d3.select('#tooltip')
+          .style('left', (d3.event.pageX + 10) + 'px')
+          .style('top', (d3.event.pageY + 10) + 'px')
+          .style('opacity', 1)
+          .html(`<strong>${this.country}</strong>`)
+      }
+    },
+
+    getCountry (coords, countries) {
+      const pos = this.projection.invert(coords)
+      return countries.find((f) => {
+        return f.geometry.coordinates.find((c1) => {
+          return this.polygonContains(c1, pos) || c1.find((c2) => {
+            return this.polygonContains(c2, pos)
+          })
+        })
+      })
+    },
+
+    polygonContains (polygon, point) {
+      const n = polygon.length
+      let p = polygon[n - 1]
+      const [x, y] = [point[0], point[1]]
+      let [x0, y0] = [p[0], p[1]]
+      let x1, y1
+      let inside = false
+      for (let i = 0; i < n; ++i) {
+        p = polygon[i];
+        [x1, y1] = [p[0], p[1]]
+        if (((y1 > y) !== (y0 > y)) && (x < (x0 - x1) * (y - y1) / (y0 - y1) + x1)) {
+          inside = !inside
+        }
+        [x0, y0] = [x1, y1]
+      }
+
+      return inside
+    },
+
+    render (land, borders, country) {
+      const context = this.context
+      context.clearRect(0, 0, this.width, this.height)
+
+      context.beginPath()
+      this.path(land)
+      context.fillStyle = '#000'
+      context.fill()
+
+      context.beginPath()
+      this.path(country)
+      context.fillStyle = '#fff'
+      context.fill()
+      context.lineWidth = 0.5
+      context.strokeStyle = '#f00'
+      context.stroke()
+
+      context.beginPath()
+      this.path(borders)
+      context.strokeStyle = '#fff'
+      context.lineWidth = 0.5
+      context.stroke()
+
+      context.beginPath()
+      this.path(this.sphere)
+      context.strokeStyle = '#000'
+      context.lineWidth = 1.5
+      context.stroke()
+
+      return context.canvas
+    },
+
+    drag (projection) {
+      let v0, q0, r0
+      const dragstarted = () => {
+        v0 = versor.cartesian(this.projection.invert([d3.event.x, d3.event.y]))
+        q0 = versor(r0 = this.projection.rotate())
+      }
+
+      const dragged = () => {
+        const v1 = versor.cartesian(this.projection.rotate(r0).invert([d3.event.x, d3.event.y]))
+        const q1 = versor.multiply(q0, versor.delta(v0, v1))
+        this.projection.rotate(versor.rotation(q1))
+      }
+      return d3.drag()
+        .on('start', dragstarted)
+        .on('drag', dragged)
+    }
+
+  }
+}
+
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+#tooltip {
+  line-height: 1;
+  font-weight: bold;
+  padding: 12px;
+  background: rgba(0, 0, 0, 0.8);
+  color: #fff;
+  border-radius: 2px;
+  font-size: 12px;
+}
+#tooltip > strong {
+  font-size: 14px;
+}
+</style>
